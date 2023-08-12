c77ccc332889360d426cc2da8d78bd3f44f54bc1
holotape <7445231+holotape@users.noreply.github.com>
2023-08-12 15:14:20 -0400

    Mandelbaum! Mandelbaum! Mandelb(rot)!

--- app.py
+++ app.py
@@ -24,12 +24,15 @@ def generate_fractal(xmin, xmax, ymin, ymax):
 fig, ax = plt.subplots()
 im = ax.imshow(generate_fractal(-2.0, 1.0, -1.5, 1.5), extent=(-2.0, 1.0, -1.5, 1.5), animated=True)
 
+zoom_coordinates = [(-0.747, 0.107), (-0.748, 0.107)]
+
 def update(frame):
-    zoom_level = frame / 100
-    zoom_x = -0.95  # X coordinate for zoom focus
-    zoom_y = 0.25    # Y coordinate for zoom focus
-    range_x = 2.5 * (1 - zoom_level)
-    range_y = 2.5 * (1 - zoom_level)
+    index = frame // 500 % 2
+    zoom_x, zoom_y = zoom_coordinates[index]
+    zoom_level = (frame % 500) / 500 * 3
+    
+    range_x = 1.5 * (1 - zoom_level)
+    range_y = 1.5 * (1 - zoom_level)
     new_xmin = zoom_x - range_x
     new_xmax = zoom_x + range_x
     new_ymin = zoom_y - range_y
@@ -37,6 +40,5 @@ def update(frame):
     im.set_array(generate_fractal(new_xmin, new_xmax, new_ymin, new_ymax))
     return im,
 
-ani = FuncAnimation(fig, update, frames=200, interval=50, blit=True)
-
+ani = FuncAnimation(fig, update, frames=1000, interval=2, blit=True) # 1000 frames for example
 plt.show()
